0f20da9681d186a7c1a3e9ddbffca6ed264b318c
wangjj <wangjj886688@163.com>
2015-12-11 15:14:07 +0800

    debug

--- C_week4_task/C_week4_task/main.cpp
+++ C_week4_task/C_week4_task/main.cpp
@@ -13,10 +13,12 @@ int main(int argc, char** argv)
 	cin >> num1 >> num2;
 	CBigInt bi1(num1);
 	CBigInt bi2(num2);
+
+	cout << "加法结果：" << (bi1 + bi2) << endl;
 	cout << "第一个操作数是：" << bi1 << endl;
 	cout << "第二个操作数是：" << bi2 << endl;
 	cout << "乘法结果:" << bi1 * bi2 << endl;
-	cout << "加法结果：" << (bi1 + bi2) << endl;
+	
 	cout << "减法结果：" << (bi1 - bi2) << endl;
 	cout << "除法的结果是：" << bi1 / bi2 << endl;
 	cout << "取模的结果是：" << bi1 % bi2 << endl;
