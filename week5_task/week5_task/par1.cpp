2933f744bd49e39299dcda298849c99d273463ea
wangjj <wangjj886688@163.com>
2015-12-14 23:21:17 +0800

    week5

---
+++ week5_task/week5_task/par1.cpp
@@ -0,0 +1,14 @@
+class MyString :public string
+{
+public:
+	MyString() : string(){};
+	MyString(const char *s) :string(s){
+
+	};
+	MyString(const string&s) :string(s){};
+	MyString operator()(int i, int j){
+		return this->substr(i, j);
+	}
+private:
+
+};
